600117050ddc994202ea38fa66a16d9de8664e55
Sapodilla <jakebaxter8@hotmail.co.uk>
2023-05-16 00:26:58 +0100

    Added car information into Car.cs

--- Assets/_Project/Scripts/Car/Car.cs
+++ Assets/_Project/Scripts/Car/Car.cs
@@ -1,13 +1,76 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using System.Text;
 using UnityEngine;
+
+
+
+
+[Serializable]
+public class PartColor
+{
+    [SerializeField] public Color color;
+    [SerializeField] public string colorName;
+    [SerializeField] public float price;
+}
+
+
+[Serializable]
+public class CarPart
+{
+    [SerializeField] public string DisplayPartName;
+    [SerializeField] public PartColor[] colors;
+    public PartColor activeColor;
+    [SerializeField] public MeshRenderer PartMesh;
+
+    private MaterialPropertyBlock m_MaterialPropertyBlock;
+
+
+    public void ChangeColor(Color color)
+    {
+        m_MaterialPropertyBlock= new MaterialPropertyBlock();
+        m_MaterialPropertyBlock.SetColor("_BaseColor", color);
+        PartMesh.SetPropertyBlock(m_MaterialPropertyBlock);
+    }
+
+    public void SetActive(PartColor color)
+    {
+        activeColor = color;
+    }
+
+}
+
+[Serializable]
+public struct CarSpecifications
+{
+
+    public string EngineType;
+    public string Displacement;
+    public string Horsepower;
+    public string Torque;
+
+
+    public string PrintSpecifications()
+    {
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.Append("Engine type: " + EngineType + "<br>");
+        sb.Append("Displacement: " + Displacement + "<br>");
+        sb.Append("Horsepower: " + Horsepower + "<br>");
+        sb.Append("Torque: " + Torque + "<br>");
+
+        return sb.ToString();
+    }
+
+}
+
 
 public class Car : MonoBehaviour
 {
 
-    [SerializeField] CarObject carData;
+    [SerializeField] public string CarDisplayName;
+    [SerializeField] public CarPart[] parts;
 
-    CarPartStruct[] Parts;
-
+    [SerializeField] public CarSpecifications specifications;
 
 }
